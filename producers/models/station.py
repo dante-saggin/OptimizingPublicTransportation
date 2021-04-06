4511cdd288e68c5f2dbd219a4bb42f6807ebe8cc
Dante Saggin <dante.saggin@gmail.com>
2021-04-06 23:09:39 +0100

    Update station.py

--- producers/models/station.py
+++ producers/models/station.py
@@ -42,8 +42,8 @@ class Station(Producer):
             topic_name,
             key_schema=Station.key_schema,
             value_schema=Station.value_schema, 
-            num_partitions=2,
-            num_replicas=2,
+            num_partitions=1,
+            num_replicas=1,
         )
 
         self.station_id = int(station_id)
