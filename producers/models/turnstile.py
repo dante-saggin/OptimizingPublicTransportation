58e456778750e3687349647b315ce83083dd1d65
Dante Saggin <dante.saggin@gmail.com>
2021-04-06 22:31:05 +0100

    Update turnstile.py

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -38,7 +38,7 @@ class Turnstile(Producer):
         #
         #
         super().__init__(
-            "org.chicago.cta.station.turnstile.v1", # DONE: Come up with a better topic name
+            "org.chicago.cta.turnstile.v1", # DONE: Come up with a better topic name
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema, #DONE: Uncomment once schema is defined
             num_partitions=2,
